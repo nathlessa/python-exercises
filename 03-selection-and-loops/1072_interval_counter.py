39bd22c42e43de8113c4d9499f7f3ce71d3e7844
nathlessa <nathlessasa@gmail.com>
2026-03-04 10:57:12 -0300

    adding 1072_interval_counter exercise

---
+++ 03-selection-and-loops/1072_interval_counter.py
@@ -0,0 +1,13 @@
+valor = int(input())
+dentro=0
+fora=0
+
+for numero in range(valor):
+    x = int(input())
+    if x >=10 and x <=20:
+        dentro = dentro + 1
+    else:
+        fora = fora + 1
+
+print(f"{dentro} in")
+print(f"{fora} out")
